ea66c4e3f4b32e169f59888d385d8f969e8c1bbb
Moises Ortega Rodriguez <Moises.Ortega@col.vueling.com>
2020-05-12 12:34:00 +0200

    changes and email message added

--- cypress/integration/exampleTest.e2e.ts
+++ cypress/integration/exampleTest.e2e.ts
@@ -7,12 +7,19 @@ import { OrderPage } from "../webpages/orderPage"
 
 describe('Example Auto Test', function () {
     var example: any
+    let emailData: any = null
 
     beforeEach(() => {
         cy.server()
         cy.fixture('ExampleData').as('exData').then(($ex) => {
             example = $ex
         })
+        cy.task('getEmail',{ timeout: 10000 })
+            .as('email')
+            .then(e => {
+                emailData = e
+                cy.log("Email String: " + emailData)
+            })
         cy.visit('/')
     })
 
@@ -21,7 +28,7 @@ describe('Example Auto Test', function () {
         cy.screenshot()
     })
 
-    it('Basic Test', function () {
+    xit('Basic Test', function () {
         var exampleData = new ExampleData(example.name, example.email, example.body, example.search)
 
         //console.error("aasdasdas")
@@ -31,7 +38,7 @@ describe('Example Auto Test', function () {
         OrderPage.PriceAsserts()
     })
 
-    it('Get and check console log Browser', function () {
+    xit('Get and check console log Browser', function () {
         console.error("aasdasdas")
 
         cy.task('getEventError')
@@ -44,5 +51,9 @@ describe('Example Auto Test', function () {
                 //console.error not shows
                 expect(hasError).to.be.false
             })
+    })
+
+    it('Email Test', () => {
+        cy.log(emailData)
     })
 })
